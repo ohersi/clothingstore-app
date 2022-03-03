41c78d7b88f7a3ba6a60aae7d0a716cfcd6d14f9
Omar Hersi <o.hersi@yahoo.com>
2022-03-03 16:22:35 -0500

    added react-router-dom, added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+// Pages
+import Home from './pages/Home';
+import Products from './pages/Products';
 // CSS
 import './App.css';
 
 const App = () => {
-  
+
   return (
     <div>
+      <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/' element={<Products />} />
+      </Routes>
       Hello
     </div>
   );
