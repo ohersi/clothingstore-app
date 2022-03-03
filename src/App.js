6027f2142cac6a8fa4c382aaa306ba943ac07acf
Omar Hersi <o.hersi@yahoo.com>
2022-03-03 16:28:44 -0500

    created components and Nav folder, js file and added links to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
+// Components
+import Nav from './components/Nav';
 // Pages
 import Home from './pages/Home';
 import Products from './pages/Products';
@@ -9,11 +11,11 @@ const App = () => {
 
   return (
     <div>
-      <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/' element={<Products />} />
+      <Nav />
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='products' element={<Products />} />
       </Routes>
-      Hello
     </div>
   );
 }
